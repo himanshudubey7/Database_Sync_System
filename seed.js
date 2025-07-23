83a59641a827b12e0be0a5c48178464fa6ada7b3
Himanshu Dubey <arjund030@gmail.com>
2025-07-23 17:31:18 +0530

    updated seed.js and updated mongodb uri

--- seed.js
+++ seed.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const { MongoClient } = require("mongodb");
 
-const localUri = "mongodb://127.0.0.1:27017";  // Local MongoDB
-const cloudUri = process.env.MONGO_URI;        // Cloud MongoDB URI from .env
+const localUri = "mongodb://127.0.0.1:27017";  
+const cloudUri = process.env.MONGO_URI;      
 
 const sampleLocalData = [
   { _id: 1, name: "Himanshu1", updatedAt: new Date("2025-07-22T10:00:00Z"), source: "LOCAL" },
@@ -30,9 +30,9 @@ async function seedDB() {
     await localDB.collection("local_data").insertMany(sampleLocalData);
     await cloudDB.collection("cloud_data").insertMany(sampleCloudData);
 
-    console.log("✅ Seeded both local and cloud data successfully.");
+    console.log("Seeded both local and cloud data successfully.");
   } catch (err) {
-    console.error("❌ Error seeding databases:", err);
+    console.error(" Error seeding databases:", err);
   } finally {
     await localClient.close();
     await cloudClient.close();
